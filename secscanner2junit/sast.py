eff9515c410df40b67f357c6753795d5c47be814
Nicolas SIMON <nicolas.simon@sciores.fr>
2022-11-03 18:44:08 +0100

    Add Newline to RESOLUTION, put back ID

--- secscanner2junit/sast.py
+++ secscanner2junit/sast.py
@@ -72,7 +72,7 @@ class SastParser(Parser):
         
         try:
             for identifier in finding['identifiers']:
-                output += "--------- RESOLUTION ----------"
+                output += "--------- RESOLUTION --------\n"
                 output += "identifiers.name: {identifiers_name}\n".format(identifiers_name=identifier['name'])
                 output += "identifiers.type: {identifiers_type}\n".format(identifiers_type=identifier['type'])
                 output += "identifiers.value: {identifiers_value}\n".format(identifiers_value=identifier['value'])
@@ -88,9 +88,9 @@ class SastParser(Parser):
             finding_id = str(randrange(1, 10000000))
         
         if properties['name']:
-            tc = TestCase(name=properties['severity']  + " - " + properties['name'] , classname=self.p_type, file=properties['file'], elapsed_sec=time, line=properties['start_line'])
+            tc = TestCase(name=properties['severity']  + " - " + properties['name']  + "(ID : " + properties['id'] + ")", classname=self.p_type, file=properties['file'], elapsed_sec=time, line=properties['start_line'])
         else:
-            tc = TestCase(name=properties['severity']  + " - " + f_type , classname=self.p_type, file=properties['file'], elapsed_sec=time, line=properties['start_line'])
+            tc = TestCase(name=properties['severity']  + " - " + f_type + "(ID : " + properties['id'] + ")", classname=self.p_type, file=properties['file'], elapsed_sec=time, line=properties['start_line'])
         tc.add_failure_info(message=properties['message'], output=output, failure_type=f_type)
         return tc
 
